7a35609693f65a6c203a15b45d516db4016230da
Diego Cabo <dcgdiego@gmail.com>
2019-01-14 19:41:26 +0100

    plot arrows

--- Lab6/src/lab6_1.m
+++ Lab6/src/lab6_1.m
@@ -10,16 +10,36 @@ k = 2
 type = 'normal';
 % type = '++';
 
-[w,clustering] = kmeans(k, dataset, type);
-scatter(dataset(:,1), dataset(:,2),[],clustering)
+[w,clustering, centroids] = kmeans(k, dataset, type);
 
+maxNumClusters = max(clustering);
+group = {}
+
+for i = 1:maxNumClusters
+    idx = find(clustering==i);
+    group{end+1} = dataset(idx,:);
+end
+Markers = ['+','o','*','x','v','d','^','s','>','<'];
+Colors = {'r', 'g', 'b', 'y', 'm', 'c', [0.5 0.5 0.5], 'k'};
+% Colors = ['k', 'k', 'k', 'k', 'k', 'k', 'k', 'k'];
+for i = 1:maxNumClusters
+    scatter(group{i}(:,1), group{i}(:,2),[],Colors{i},Markers(i))
+    hold on;
+end
 for i = 1:k
     hold on;
-    scatter(w(i,1),w(i,2),150,i,'filled','MarkerEdgeColor','k','LineWidth',1)
+    scatter(w(i,1),w(i,2),150,Colors{i},'filled','MarkerEdgeColor','k','LineWidth',1)
+end
+
+for i = 1:k
+    for j = 2:length(centroids)
+        hold on;
+        plot_arrow(centroids{j-1}(i,1), centroids{j-1}(i,2), centroids{j}(i,1), centroids{j}(i,2))
+    end
 end
 
 
-function [w, clustering] = kmeans(k, dataset, type)
+function [w, clustering, centroids] = kmeans(k, dataset, type)
 n_points = length(dataset);
 % which cluster does the datapoint belongs to
 clustering = zeros(n_points,1);
@@ -31,6 +51,8 @@ else
     idx = randperm(n_points,k);
     w = dataset(idx,:); 
 end
+
+centroids = {w};
 
 no_change = 1;
 while no_change
@@ -50,7 +72,8 @@ while no_change
     for h = 1:k
        w(h,:) = mean(dataset(clustering==h,:));
     end
-    
+    % store
+    centroids{end+1} = w;
     if clustering == old_clustering
         no_change = 0;
     end    
