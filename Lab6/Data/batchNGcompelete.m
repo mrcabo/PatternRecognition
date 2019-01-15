436e9c027624b122a162b5216f49bb8cc92f7791
unknown <na.mozilla@gmail.com>
2019-01-15 21:20:50 +0100

    data file added

--- Lab6/Data/batchNGcompelete.m
+++ Lab6/Data/batchNGcompelete.m
@@ -2,6 +2,8 @@ function [prototypes] = batchNG(Data, n, epochs, xdim, ydim)
 
 % Batch Neural Gas
 %   Data contains data,
+Data = load ('data/checkerboard.mat');
+%portotypes = load ('clusterCentroids.mat')
 %   n is the number of clusters,
 %   epoch the number of iterations,
 %   xdim and ydim are the dimensions to be plotted, default xdim=1,ydim=2
@@ -9,7 +11,7 @@ function [prototypes] = batchNG(Data, n, epochs, xdim, ydim)
 error(nargchk(3, 5, nargin));  % check the number of input arguments
 if (nargin<4)
   xdim=1; ydim=2;   % default plot values
-end;
+end
 
 [dlen,dim] = size(Data);
 
@@ -21,24 +23,46 @@ prototypes=fromfile('clusterCentroids.mat');
 
 lambda0 = n/2; %initial neighborhood value
 % lambda
-lambda = lambda0 * (0.01/lambda0).^([0:(epochs-1)]/epochs);
+lambda = lambda0 * (0.01/lambda0).^(0:(epochs-1)/epochs);
 % note: the lecture slides refer to this parameter as sigma^2
 %       instead of lambda
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %% Action
 
-for i=1:epochs,
+for i=1:epochs
   D_prototypes = zeros(n,dim);   % difference for vectors is initially zero
   D_prototypes_av = zeros(n,1);       % the same holds for the quotients
   
-  for j=1:dlen,  % consider all points at once for the batch update
+  for j=1:dlen  % consider all points at once for the batch update
     
     % sample vector
     x = Data(j,:); % sample vector
     X = x(ones(n,1),:);  % we'll need this
     
     % neighborhood ranking
+    no_change = 1;
+    while no_change
+        old_clustering = clustering;
+        d = zeros(n_points,k);
+        for j = 1:k
+            d(:,j) = pdist2(dataset, prototypes(j,:), 'squaredeuclidean');
+        end
+        [M,I] = min(d,[],2);
+        [B, idx] = sort(M,2);
+        %3closest = idx(
+        
+        clustering = I;
+        % recalculate centroids
+        for h = 1:k
+           prototypes(h,:) = mean(dataset(clustering==h,:));
+        end
+        % store
+        centroids{end+1} = prototypes;
+        if clustering == old_clustering
+            no_change = 0;
+        end    
+    end
     % DISTANCE!!!
     % 1-BMU, 2-BMU, etc. (hint:sort)
     %find ranking,h,H
@@ -47,7 +71,7 @@ for i=1:epochs,
     D_prototypes = ...
     D_prototypes_av = ...
   end
-  D_prototypes = ...
+  D_prototypes = D_prototypes/D_prototypes_av
       
   % update
   prototypes = D_prototypes ;
